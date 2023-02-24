624769baa7084db52c714e18b6ec4af9284338b9
mukeshsin <110765718+mukeshsin@users.noreply.github.com>
2023-02-24 12:06:29 +0530

    firstStep done

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <form>
+      <h1 class="searchTag">Search hashtag</h1>
+      <input class="inputField" type="text" v-model="searchTerm" />
+    </form>
+    <div class="cardWrapper">
+      <blogCard v-for="blogData in blogDatas" :key="blogData.id">
+        <template v-slot:title>{{ blogData.title }}</template>
+        <template v-slot:description>{{ blogData.description }}</template>
+        <template v-slot:footer>
+          <div class="likes">
+            <div class="icon" @click="increaseLike(blogData.id)">
+              <i class="fa-sharp fa-solid fa-heart"></i>
+            </div>
+            <p>{{ blogData.like }}</p>
+          </div>
+          <div v-for="hashtag in blogDatas.hashtags" :key="hashtag">{{ hashtag }}</div>
+        </template>
+      </blogCard>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import  blogCard  from "./components/cards.vue";
+
+export default {
+  name: "App",
+  components: {
+    blogCard
+  },
+  data() {
+    return {
+      blogDatas: [
+          {
+            id: 1,
+            title: "Learning Vue js 3",
+            content:
+              "I am Learning Vue js 3 with the composition API.it is great",
+            like: 5,
+            hashtags: ["#Vue", "#Javascript", "#CompositionApi"],
+          },
+          {
+            id: 2,
+            title: "Learning Vuex",
+            content:
+              "vuex is a state management solution for Vue .its allows you to logically separate entities into Modules.",
+            like: 3,
+            hashtags: ["#Vue", "#Vuex", "#Flux"],
+          },
+          {
+            id: 3,
+            title: "Routing With Vue Router",
+            content: "I am creating complex front-end app using vue-router",
+            like: 1,
+            hashtags: ["#Vue", "#VueRouter"],
+          },
+          {
+            id: 4,
+            title: "Testing Vue Apps",
+            content:
+              "I am writing Some Tests for My application using test utils. Testing is critical but often overlooked due to complexity or time constraints",
+            like: 1,
+            hashtags: ["#Vue", "#Javascript", "#Testing"],
+          },
+        ],
+      searchTerm: "",
+    };
+  },
+ 
+};
+</script>
+
+<style>
+.cardWrapper {
+  border: 2px solid black;
+  width: 200px;
+  height: 300px;
+}
+</style>
---
+++ src/components/cards.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="cards">
+    <div class="card-header">
+        <slot name="title"></slot>
+    </div>
+
+    <div class="card-content">
+        <slot name="description"></slot>
+
+    </div>
+
+    <div class="card-footer">
+        <slot name="likes"></slot>
+        <slot name="hashTags"></slot>
+
+    </div>
+
+</div>
+</template>
+
+<script>
+export default {
+    name: "blogCard",
+}
+
+</script>
+
+<style scoped>
+.cards {
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+    cursor: pointer;
+    margin: 10px;
+    width: 400px;
+    float: right;
+}
+
+.card-header {
+    background-color: #eee;
+    border-bottom: 2px solid #ccc;
+    font-weight: bold;
+    font-size: 20px;
+    padding: 10px;
+    text-align: center;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+.card-content {
+    display: flex;
+    justify-content: center;
+    padding: 10px;
+}
+
+.card-footer {
+    border-top: 1px solid #ccc;
+    padding: 10px;
+    text-align: center;
+    height: 40px;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-weight: 500;
+
+}
+</style>
